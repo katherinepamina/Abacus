948c87bbcf7d5bd3d983e642f732a93c6055af95
katherinepamina <katherinelin2016@u.northwestern.edu>
2016-05-27 16:18:03 -0500

    Kathy commits probly broken

--- server/src/integrated.py
+++ server/src/integrated.py
@@ -12,35 +12,7 @@ pusher_client = pusher.Pusher(
 cap = cv2.VideoCapture(1)
 
 
-# Capture frame-by-frame
-#ret, frame = cap.read()
-
-# Our operations on the frame come here
-#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-# draw a rectangle
-#cv2.rectangle(frame, (19, 27), (1279, 959), (0, 255, 0), 3)
-#corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
-#corners = np.int0(corners)
-
-        #for i in corners:
-#    x, y = i.ravel()
-#    cv2.circle(gray, (x, y), 3, 255, -1)
-# Display the resulting frame
-#cv2.imshow('frame',frame)
-#if cv2.waitKey(1) & 0xFF == ord('q'):
-#    break
-
-# height, width, depth = frame.shape
-# #blue dot rgb value: 124, 182, 239
-# for i in range(0, height):
-#     for j in range(0, width):
-#         if frame[i, j][0] > 200 and frame[i,j][1]<200 and frame[i,j][2]<200 and frame[i, j][0] < 250 and frame[i,j][1]>150 and frame[i,j][2]>100:
-#             #print frame[i,j]
-#             print "x = " + str(i)
-#             print "y = " + str(j)
-#             print "========="
-
+#set(CV_CAP_PROP_FPS,30);
 
 while(True):
     # Capture frame-by-frame
@@ -141,7 +113,7 @@ while(True):
 
     final_sum = sum
     pusher_client.trigger('abacus_channel', 'updated', final_sum)
-    print final_sum
+    #print final_sum
     #print("done")
 
     #corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
